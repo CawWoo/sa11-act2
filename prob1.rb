882854bc569e787470f65c03e3c23e77bf34dd46
CawWoo <melodicraven@gmail.com>
2024-03-21 20:59:17 -0500

    Solved problem 1

--- prob1.rb
+++ prob1.rb
@@ -1,7 +1,7 @@
 #1.	Extracting Hashtags from a Tweet Replace nil with the regular expression to extract all hashtags.
 
 def extract_hashtags(text)
-  pattern = nil
+  pattern = /#[\w]+/
   hashtags = text.scan(pattern)
   hashtags.flatten
 end
